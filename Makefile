80c49bcce34273d9a5ad4bd7aac14d96a4dac64f
zekepinkerton <133540670+zekepinkerton@users.noreply.github.com>
2025-02-19 22:42:42 +0000

    Step2 Complete

--- Makefile
+++ Makefile
@@ -23,7 +23,7 @@ DEBUG ?= -g
 SANATIZE ?= -fno-omit-frame-pointer -fsanitize=address
 
 #If you need to link against a library uncomment the line below and add the library name
-#LDFLAGS ?= -pthread -lreadline
+LDFLAGS ?= -pthread -lreadline
 
 #Default to building without debug flags
 all: $(TARGET_EXEC) $(TARGET_TEST)
--- src/lab.c
+++ src/lab.c
@@ -1,1 +1,54 @@
-/**Update this file with the starter code**/
+#include "lab.h"
+#include <stdio.h>
+#include <getopt.h>
+
+char *get_prompt(const char *env) {
+    // Implementation here
+    return NULL;
+}
+
+int change_dir(char **dir) {
+    // Implementation here
+    return 0;
+}
+
+char **cmd_parse(char const *line) {
+    // Implementation here
+    return NULL;
+}
+
+void cmd_free(char **line) {
+    // Implementation here
+}
+
+char *trim_white(char *line) {
+    // Implementation here
+    return line;
+}
+
+bool do_builtin(struct shell *sh, char **argv) {
+    // Implementation here
+    return false;
+}
+
+void sh_init(struct shell *sh) {
+    // Implementation here
+}
+
+void sh_destroy(struct shell *sh) {
+    // Implementation here
+}
+
+void parse_args(int argc, char **argv) {
+    int opt;
+    while ((opt = getopt(argc, argv, "v")) != -1) {
+        switch (opt) {
+            case 'v':
+                printf("Shell version %d.%d\n", lab_VERSION_MAJOR, lab_VERSION_MINOR);
+                exit(EXIT_SUCCESS);
+            default:
+                fprintf(stderr, "Usage: %s [-v]\n", argv[0]);
+                exit(EXIT_FAILURE);
+        }
+    }
+}
